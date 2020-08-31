c7760ec03011727ea5b1df25a61ed9b36e789d83
Paa kwasi Oppong <34088228+paakwasi317@users.noreply.github.com>
2020-08-31 00:33:55 +0000

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,11 +123,4 @@ STATIC_URL = '/static/'
 
 ASGI_APPLICATION = 'mysite.routing.application'
 
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
-}
+
